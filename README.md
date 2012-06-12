ee0bf60275d312510c31126a0562b6ccde9cc525
Matt Hernandez <matt@eventargs.com>
2012-06-12 09:01:21 -0400

    Removed the coloring for grid roles. Updated the readme.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-Bokmarklet to add style to show which elements have ARIA roles and
-are present in the tab index.  Other highlights per pull requests :)
+# ARIA Highlighter Bookmarklet
+A bookmarklet that highlights the elements with roles and shows which roles the elements use.
+
+The following items are highlighted/shown:
+* Any element with an ARIA role attribute will be highlighted pink
+* The ARIA role value will be placed before the element (in green)
+* Elements with the "row" role will be outlined
+* Elements wth the "gridcell" role will be highlighted light red
--- src/aria-highlighter.js
+++ src/aria-highlighter.js
@@ -29,12 +29,8 @@
 
 */
 
-// TODO: Add a legend and color code rows and gridcells
-
 (function (document) {
-  'use strict';
-  
-  var css = '[tabindex=0]{background:yellow}[tabindex]:before{content:attr(tabindex);color:red;font-weight:bold}[role]{background:rgba(255,191,202,0.4)}[role]:before{content:attr(role); color:green;font-weight:bold}[role=grid]{background-color:rgba(255,167,0,0.3);}[role=row]{border: 1px solid rgba(0,0,0,0.5);}[role=gridcell]{background-color:rgba(255,0,0,0.3);}';
+  var css = '[tabindex=0]{background:yellow}[tabindex]:before{content:attr(tabindex);color:red;font-weight:bold}[role]{background:rgba(255,191,202,0.4)}[role]:before{content:attr(role); color:green;font-weight:bold}[role=row]{border: 1px solid rgba(0,0,0,0.5);}[role=gridcell]{background-color:rgba(255,0,0,0.3);}';
   var styleElem = document.getElementById('aria-highlighter');
     
   if (styleElem) {
